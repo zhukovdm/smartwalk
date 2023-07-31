7ccf8b9236e5918e4473fa643f04e4aa4c53b128
Dmitry Zhukov <zhukovdm@proton.me>
2023-07-31 17:32:19 +0200

    init backend domain proj

---
+++ app/backend/SmartWalk.Domain/Entities/Bound.cs
@@ -0,0 +1,47 @@
+using System.ComponentModel.DataAnnotations;
+
+using CollectBound = System.Collections.Generic.List<string>;
+
+namespace SmartWalk.Domain.Entities;
+
+public sealed class NumericBound
+{
+    [Required]
+    public double min { get; set; }
+
+    [Required]
+    public double max { get; set; }
+}
+
+public sealed class BoundObject
+{
+    [Required]
+    public NumericBound capacity { get; set; }
+
+    [Required]
+    public NumericBound elevation { get; set; }
+
+    [Required]
+    public NumericBound minimumAge { get; set; }
+
+    [Required]
+    public NumericBound rating { get; set; }
+
+    [Required]
+    public NumericBound year { get; set; }
+
+    [Required]
+    public CollectBound clothes { get; set; }
+
+    [Required]
+    public CollectBound cuisine { get; set; }
+
+    [Required]
+    public CollectBound denomination { get; set; }
+
+    [Required]
+    public CollectBound payment { get; set; }
+
+    [Required]
+    public CollectBound rental { get; set; }
+}
