3b2b2c5ab18e90823bca943587998487c24b9163
Dmitry Zhukov <zhukovdm@proton.me>
2023-08-14 20:07:20 +0200

    add list extensions (merge func.)

---
+++ app/backend/SmartWalk.Domain/Extensions/ListExtensions.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using SmartWalk.Domain.Entities;
+
+public static class ListExtensions
+{
+    public static List<Place> WithMergedCategories(this List<Place> places)
+    {
+        var result = new Dictionary<string, Place>();
+
+        foreach (var place in places)
+        {
+            if (result.TryGetValue(place.smartId, out var p))
+            {
+                p.categories.UnionWith(place.categories);
+            }
+            else { result.Add(place.smartId, place); }
+        }
+        return result.Values.ToList();
+    }
+}
