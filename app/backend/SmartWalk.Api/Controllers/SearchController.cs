75eb205ee5a0d579d09a9311bdae737338c640c4
Dmitry Zhukov <zhukovdm@proton.me>
2023-08-15 15:31:31 +0200

    route query with precedence

--- app/backend/SmartWalk.Api/Controllers/SearchController.cs
+++ app/backend/SmartWalk.Api/Controllers/SearchController.cs
@@ -201,7 +201,7 @@ public sealed class SearchController : ControllerBase
     public sealed class RoutesRequest
     {
         /// <example>
-        ///   {"source":{"lon":14.4035264,"lat":50.0884344},"target":{"lon":14.4039444,"lat":50.0894092},"distance":900,"categories":[{"keyword":"museum","filters":{}},{"keyword":"tourism","filters":{}}],"precedence":[]}
+        ///   {"source":{"lon":14.4035264,"lat":50.0884344},"target":{"lon":14.4039444,"lat":50.0894092},"distance":3000,"categories":[{"keyword":"castle","filters":{}},{"keyword":"restaurant","filters":{}},{"keyword":"tourism","filters":{}}],"precedence":[{"fr":0,"to":2}]}
         /// </example>
         [Required]
         [MinLength(1)]
