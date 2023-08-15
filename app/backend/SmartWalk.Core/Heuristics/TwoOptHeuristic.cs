540c3c46c016ef4b1d50720d88141ca2591150b7
Dmitry Zhukov <zhukovdm@proton.me>
2023-08-15 13:04:30 +0200

    add 2-opt heuristic

--- app/backend/SmartWalk.Core/Heuristics/TwoOptHeuristic.cs
+++ app/backend/SmartWalk.Core/Heuristics/TwoOptHeuristic.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using SmartWalk.Domain.Entities;
 using SmartWalk.Domain.Interfaces;
 
 namespace SmartWalk.Core.Heuristics;
@@ -12,35 +13,35 @@ namespace SmartWalk.Core.Heuristics;
 internal static class TwoOptHeuristic
 {
     /// <summary>
-    /// The first
-    /// and last items are not swappable. Used as a refinement step for routes
-    /// without precedence constraints.
+    /// Used as a refinement step for routes without precedence constraints.
+    /// The first and last items are never swapped.
     /// </summary>
-    public static List<int> Refine(List<int> route, IDistanceMatrix matrix)
+    public static List<SolverPlace> Refine(List<SolverPlace> seq, IDistanceMatrix matrix)
     {
         bool change;
         do
         {
             change = false;
 
-            for (int i = 0; i < route.Count - 3; ++i)
+            for (int i = 0; i < seq.Count - 3; ++i)
             {
-                for (int j = i + 1; j < route.Count - 2; ++j)
+                for (int j = i + 1; j < seq.Count - 2; ++j)
                 {
                     double diff = 0.0
-                        - matrix.Distance(route[i    ], route[i + 1])
-                        - matrix.Distance(route[j    ], route[j + 1])
-                        + matrix.Distance(route[i    ], route[j    ])
-                        + matrix.Distance(route[i + 1], route[j + 1]);
+                        - matrix.GetDistance(seq[i    ].Idx, seq[i + 1].Idx)
+                        - matrix.GetDistance(seq[j    ].Idx, seq[j + 1].Idx)
+                        + matrix.GetDistance(seq[i    ].Idx, seq[j    ].Idx)
+                        + matrix.GetDistance(seq[i + 1].Idx, seq[j + 1].Idx);
 
-                    if (diff < 0)
+                    if (diff < 0.0)
                     {
-                        change = true; route.Reverse(i + 1, j - i);
+                        change = true;
+                        seq.Reverse(i + 1, j - i);
                     }
                 }
             }
         } while (change);
 
-        return route;
+        return seq;
     }
 }
