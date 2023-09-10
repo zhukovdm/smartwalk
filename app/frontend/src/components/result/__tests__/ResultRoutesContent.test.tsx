e9ac55294a0fd62f30df69f673917ce4b334b622
Dmitry Zhukov <zhukovdm@proton.me>
2023-09-10 02:13:25 +0200

    init route result tests

--- app/frontend/src/components/result/__tests__/ResultRoutesContent.test.tsx
+++ app/frontend/src/components/result/__tests__/ResultRoutesContent.test.tsx
@@ -1,11 +1,50 @@
+import { getRoute } from "../../../utils/testData";
+import {
+  type AppRenderOptions,
+  renderWithProviders
+} from "../../../utils/testUtils";
+import {
+  initialResultRoutesState
+} from "../../../features/resultRoutesSlice";
+import ResultRoutesContent from "../ResultRoutesContent";
+
+const N = 5;
+
+const getState = (): AppRenderOptions => ({
+  preloadedState: {
+    resultRoutes: {
+      ...initialResultRoutesState(),
+      result: Array(N).fill(undefined).map(() => getRoute()),
+      resultFilters: Array(N).fill(true)
+    }
+  }
+});
+
+function render(options: AppRenderOptions = getState()) {
+  return renderWithProviders(<ResultRoutesContent />, options);
+}
+
 describe("<ResultRoutesContent />", () => {
 
   test("render", () => {
-    const { container } = { container: true }
+    const { container } = render();
     expect(container).toBeTruthy();
   });
 
   describe("header", () => {
     
+    it("should generate for =1 route", () => {
+      const { getByText } = render({
+        preloadedState: {
+          resultRoutes: {
+            ...initialResultRoutesState(),
+            result: [
+              getRoute()
+            ],
+            resultFilters: Array()
+          }
+        }
+      })
+    });
   });
 });
