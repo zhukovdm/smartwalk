a630fb9eaa6ada7f4161495de89eb517220c4a67
Dmitry Zhukov <zhukovdm@proton.me>
2023-09-21 13:41:51 +0200

    add tests for viewer panel

---
+++ app/frontend/src/components/__tests__/ViewerDirecPanel.test.tsx
@@ -0,0 +1,52 @@
+import type { StoredDirec } from "../../domain/types";
+import { getDirec } from "../../utils/testData";
+import {
+  type AppRenderOptions,
+  renderWithProviders
+} from "../../utils/testUtils";
+import { initialViewerState } from "../../features/viewerSlice";
+import { initialFavoritesState } from "../../features/favoritesSlice";
+import ViewerDirecPanel from "../ViewerDirecPanel";
+
+const getProps = (): {} => ({});
+
+const getOptions = (direc: StoredDirec | undefined): AppRenderOptions => ({
+  preloadedState: {
+    favorites: {
+      ...initialFavoritesState(),
+      loaded: true
+    },
+    viewer: {
+      ...initialViewerState(),
+      direc
+    }
+  }
+});
+
+function render(props = getProps(), options: AppRenderOptions = getOptions(undefined)) {
+  return renderWithProviders(<ViewerDirecPanel {...props} />, options);
+}
+
+describe("<ViewerDirecPanel />", () => {
+
+  test("render", () => {
+    expect(render().container).toBeTruthy();
+  });
+
+  it("should render alert if no direc is provided", () => {
+
+    const { getByRole } = render();
+    expect(getByRole("alert")).toBeVisible();
+  });
+
+  it("should render content if a direc is provided", () => {
+
+    const { getByText } = render(getProps(), getOptions({
+      ...getDirec(),
+      direcId: "1",
+      name: "Direction A"
+    }));
+
+    expect(getByText("Direction A")).toBeVisible();
+  });
+});
---
+++ app/frontend/src/components/__tests__/ViewerRoutePanel.test.tsx
@@ -0,0 +1,52 @@
+import type { StoredRoute } from "../../domain/types";
+import { getRoute } from "../../utils/testData";
+import {
+  type AppRenderOptions,
+  renderWithProviders
+} from "../../utils/testUtils";
+import { initialViewerState } from "../../features/viewerSlice";
+import { initialFavoritesState } from "../../features/favoritesSlice";
+import ViewerRoutePanel from "../ViewerRoutePanel";
+
+const getProps = (): {} => ({});
+
+const getOptions = (route: StoredRoute | undefined): AppRenderOptions => ({
+  preloadedState: {
+    favorites: {
+      ...initialFavoritesState(),
+      loaded: true
+    },
+    viewer: {
+      ...initialViewerState(),
+      route
+    }
+  }
+});
+
+function render(props = getProps(), options: AppRenderOptions = getOptions(undefined)) {
+  return renderWithProviders(<ViewerRoutePanel {...props} />, options);
+}
+
+describe("<ViewerRoutePanel />", () => {
+
+  test("render", () => {
+    expect(render().container).toBeTruthy();
+  });
+
+  it("should render alert if no route is provided", () => {
+
+    const { getByRole } = render();
+    expect(getByRole("alert")).toBeVisible();
+  });
+
+  it("should render content if a route is provided", () => {
+
+    const { getByText } = render(getProps(), getOptions({
+      ...getRoute(),
+      routeId: "1",
+      name: "Route A"
+    }));
+
+    expect(getByText("Route A")).toBeVisible();
+  });
+});
