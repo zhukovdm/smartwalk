8636aed7a202b2c5e6ad66f60d0d6a56a89d921e
Dmitry Zhukov <zhukovdm@proton.me>
2023-09-25 13:52:27 +0200

    add experiment project

---
+++ app/backend/SmartWalk.Experiment/CoreExperiments.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Linq;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using Newtonsoft.Json;
+using SmartWalk.Core.Algorithms;
+using SmartWalk.Domain.Entities;
+
+namespace SmartWalk.Experiment.Core;
+
+static class CoreExperiments
+{
+    internal static void BoundingEllipse()
+    {
+        var f1 = new WgsPoint(14.4035264, 50.0884344);
+        var f2 = new WgsPoint(14.4073961, 50.0909408);
+
+        var distance = Math.Round(Spherical.HaversineDistance(f1, f2), 0);
+
+        Console.WriteLine($"Distance between foci: {distance} meters.");
+
+        var e = Spherical.BoundingEllipse(f1, f2, 450);
+
+        var p = new Feature(
+            new Polygon(new LinearRing(e.Select((point) => new Coordinate(point.lon, point.lat)).ToArray())),
+            new AttributesTable());
+
+        var s = new Feature(
+            new Point(new Coordinate(f1.lon, f1.lat)),
+            new AttributesTable() { { "marker-color", "#2aad27" } });
+
+        var t = new Feature(
+            new Point(new Coordinate(f2.lon, f2.lat)),
+            new AttributesTable() { { "marker-color", "#cb2b3e" } });
+
+        var c = new FeatureCollection() { p, s, t };
+
+        var writer = new StringWriter();
+        {
+            using var jsonWriter = new JsonTextWriter(writer)
+            {
+                Formatting = Formatting.Indented
+            };
+            GeoJsonSerializer.Create().Serialize(jsonWriter, c);
+        }
+//      Console.WriteLine(writer);
+        File.WriteAllText("./results/core/bounding-ellipse.json", writer.ToString());
+    }
+}
