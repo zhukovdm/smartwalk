e7b7b997b837079e01005a6a4ac49934f7228a22
Dmitry Zhukov <zhukovdm@proton.me>
2023-09-25 13:52:27 +0200

    add experiment project

---
+++ app/backend/SmartWalk.Experiment/Program.cs
@@ -0,0 +1,11 @@
+using SmartWalk.Experiment.Core;
+
+namespace SmartWalk.Experiment;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CoreExperiments.BoundingEllipse();
+    }
+}
