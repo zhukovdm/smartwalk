a05dd71a0ef8304f53ba9fc96f5717ebb1c3df4d
Dmitry Zhukov <zhukovdm@proton.me>
2023-10-03 02:23:29 +0200

    add infrastructure tests

---
+++ app/backend/SmartWalk.Infrastructure.Test/OsrmDistanceMatrixQueryExecutorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartWalk.Infrastructure.Test;
+
+[TestClass]
+public class OsrmDistanceMatrixQueryExecutorTests
+{
+    [TestMethod]
+    public void ShouldPass()
+    {
+        // Class `OsrmDistanceMatrixQueryExecutor` is currently not used.
+    }
+}
