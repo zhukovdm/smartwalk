2bc29339cb16184ce4222f36b5393bbd5c426044
Dmitry Zhukov <zhukovdm@proton.me>
2023-11-01 18:10:50 +0100

    add docs for `query` parameter

---
+++ misc/query/Program.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using NJsonSchema;
+using SmartWalk.Application.Entities;
+
+namespace query;
+
+public class Program
+{
+    private static void Save(string file, string json)
+    {
+        File.WriteAllText(file, json);
+    }
+
+    public static void Main(string[] args)
+    {
+        {
+            var s = JsonSchema.FromType<SearchDirecsQuery>();
+            var j = s.ToJson(Newtonsoft.Json.Formatting.Indented);
+            Save("direcs.json", j);
+        }
+        {
+            var s = JsonSchema.FromType<SearchPlacesQuery>();
+            var j = s.ToJson(Newtonsoft.Json.Formatting.Indented);
+            Save("places.json", j);
+        }
+        {
+            var s = JsonSchema.FromType<SearchRoutesQuery>();
+            var j = s.ToJson(Newtonsoft.Json.Formatting.Indented);
+            Save("routes.json", j);
+        }
+    }
+}
