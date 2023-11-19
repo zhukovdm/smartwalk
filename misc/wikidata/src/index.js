b38b3275a67c3f51207dab98b178ee6eb2ae30f8
Dmitry Zhukov <zhukovdm@proton.me>
2023-11-19 20:32:42 +0100

    add tests for kg content negotiation

---
+++ misc/wikidata/src/index.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+// string
+// const WIKIDATA_ACCEPT_CONTENT = "text/turtle";
+
+// expanded json-ld object
+// const WIKIDATA_ACCEPT_CONTENT = "application/ld+json";
+
+// string
+const WIKIDATA_ACCEPT_CONTENT = "application/n-triples";
+
+// string
+// const WIKIDATA_ACCEPT_CONTENT = "application/n-quads";
+
+const WIKIDATA_SPARQL_ENDPOINT = "https://query.wikidata.org/sparql?query=";
+
+const wikidataQuery = () => `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX my: <http://example.com/>
+CONSTRUCT {
+  ?id my:name ?label.
+}
+WHERE {
+  ?id rdfs:label ?label.
+}
+LIMIT 10`;
+
+async function main() {
+  const response = await axios.get(WIKIDATA_SPARQL_ENDPOINT + encodeURIComponent(wikidataQuery()), {
+    headers: {
+      Accept: `${WIKIDATA_ACCEPT_CONTENT}; charset=utf-8`,
+      "User-Agent": "SmartWalk (https://github.com/zhukovdm/smartwalk)"
+    }
+  });
+  console.log(response.data);
+  console.log(typeof response.data);
+}
+
+main();
