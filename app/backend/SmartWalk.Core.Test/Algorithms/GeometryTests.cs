3dbb619b3b80ad29c8b8e3a8ad07b06dfaf05ac9
Dmitry Zhukov <zhukovdm@proton.me>
2023-11-28 15:34:27 +0100

    ensure rot-angle does not fail

--- app/backend/SmartWalk.Core.Test/Algorithms/GeometryTests.cs
+++ app/backend/SmartWalk.Core.Test/Algorithms/GeometryTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartWalk.Core.Algorithms;
 
 namespace SmartWalk.Core.Test;
 
@@ -6,7 +7,17 @@ namespace SmartWalk.Core.Test;
 public class GeometryTests
 {
     [TestMethod]
-    public void ShouldPass()
+    public void AngleIsAlwaysValid()
     {
+        for (int lon = -1; lon < 2; ++lon)
+        {
+            for (int lat = -1; lat < 2; ++lat)
+            {
+                var rads = Spherical.RotAngle(new(0.0, 0.0), new((double) lon, (double) lat));
+                var degs = Spherical.RadToDeg(rads);
+                Assert.IsTrue(degs <= +180.0);
+                Assert.IsTrue(degs >= -180.0);
+            }
+        }
     }
 }
