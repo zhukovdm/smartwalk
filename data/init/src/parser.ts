a8f02f3cbf174e4953928d7b6180453d251e6682
Dmitry Zhukov <zhukovdm@proton.me>
2023-12-04 00:52:05 +0100

    update init

---
+++ data/init/src/parser.ts
@@ -0,0 +1,13 @@
+import { Command } from "commander";
+
+export default class Parser {
+
+  /**
+   * Read connection string.
+   */
+  parseArgs(): { conn: string } {
+    const args = new Command()
+      .option("--conn [string]", "Database connection string");
+    return args.parse().opts();
+  }
+}
