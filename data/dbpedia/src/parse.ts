cd6ac838b2bfdce1757c99cd76f1edc78edb3f32
Dmitry Zhukov <zhukovdm@proton.me>
2023-12-04 00:49:52 +0100

    adapt dbpedia etl

--- data/dbpedia/src/parse.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "commander";
-
-export function parseArgs(): {
-  conn: string;
-} {
-  const args = new Command()
-    .option("--conn <string>", "Database connection string");
-  return args.parse().opts();
-}
---
+++ data/dbpedia/src/target.ts
@@ -0,0 +1,77 @@
+import {
+  EnrichLogger,
+  EnrichTarget,
+  getDateTime,
+  getPayloadIter
+} from "../../shared/src/index.js";
+
+export default class Target extends EnrichTarget {
+
+  private totalEnriched = 0;
+  private readonly logger: EnrichLogger;
+
+  constructor(logger: EnrichLogger, conn: string) {
+    super(conn);
+    this.logger = logger;
+  }
+
+  /**
+   * Get all place identifiers in the database.
+   * @param window Maximum number of items in one batch.
+   * @returns Iterator over a list of identifiers.
+   */
+  async getPayloadIter(window: number): Promise<{
+    [Symbol.iterator](): Generator<string[], void, unknown>;
+  }> {
+    const payload = await this.getPayload(window);
+    this.logger.logPayloadLength(payload.length);
+
+    return getPayloadIter(payload, window);
+  }
+
+  /**
+   * Load phase.
+   * @param items Well-formed items.
+   */
+  async l(items: any[]): Promise<void> {
+    let batchEnriched = 0;
+
+    for (const item of items) {
+      try {
+        const filter = {
+          "linked.wikidata": {
+            $eq: item.wikidata
+          }
+        };
+
+        const update: Record<string, any> = {
+          $set: {
+            "name": item.name,
+            "attributes.description": item.description,
+            "attributes.image": item.image,
+            "attributes.website": item.website,
+            "attributes.year": item.year,
+            "linked.dbpedia": item.dbpedia,
+            "linked.yago": item.yago,
+            "metadata.updated": getDateTime()
+          }
+        };
+
+        const options = {
+          ignoreUndefined: true
+        }
+
+        await this.collection.updateMany(filter, update, options);
+        ++batchEnriched;
+      }
+      catch (ex) {
+        this.logger.logFailedEnrich(item.wikidata, ex);
+      }
+    }
+
+    this.totalEnriched += batchEnriched;
+    this.logger.logItemsEnriched(batchEnriched, this.totalEnriched);
+  }
+
+  async dispose(): Promise<void> { this.client.close(); }
+}
