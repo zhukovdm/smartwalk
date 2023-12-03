66105145c3935b6622f6c0aa4215a3d80b715ae6
Dmitry Zhukov <zhukovdm@proton.me>
2023-12-03 22:57:21 +0100

    update wikidata-create etl

--- data/wikidata-create/src/index.ts
+++ data/wikidata-create/src/index.ts
@@ -1,29 +1,31 @@
-import { fetchBbox } from "./fetch";
-import Logger from "./logger";
-import Model from "./model";
-import { parseArgs } from "./parse";
+import Logger from "./logger.js";
+import Parser from "./parser.js";
+import Source from "./source.js";
+import Target from "./target.js";
 
 /**
- * Create entities that exist in the Wikidata unknown to the database.
+ * Create entities that exist in the Wikidata unknown to the entity store.
  */
 async function wikidataCreate() {
 
-  const { w, n, e, s, rows, cols, conn } = parseArgs();
-
   const logger = new Logger();
-  const model = new Model(logger, conn);
+  const { w, n, e, s, rows, cols, conn } = new Parser().parseArgs();
+
+  const source = new Source(logger);
+  const target = new Target(logger, conn);
 
   try {
     logger.logStarted();
 
-    const items = await fetchBbox(logger, { w: w, n: n, e: e, s: s }, rows, cols);
-    await model.write(items);
+    const _e = await source.e({ w, n, e, s }, rows, cols);
+    const _t = await source.t(_e);
+    const _l = await target.l(_t);
 
     logger.logFinished();
   }
   catch (ex) { logger.logError(ex); }
   finally {
-    await model.dispose();
+    await target.dispose();
   }
 }
 
---
+++ data/wikidata-create/src/parser.ts
@@ -0,0 +1,28 @@
+import { Command } from "commander";
+
+export default class Parser {
+
+  /**
+   * Define south-west, north-east bounding points, subdivision,
+   * and connection string.
+   */
+  parseArgs(): {
+    w: number;
+    n: number;
+    e: number;
+    s: number;
+    rows: number;
+    cols: number;
+    conn: string;
+  } {
+    const args = new Command()
+      .option("--w <number>", "West coordinate", parseFloat)
+      .option("--n <number>", "North coordinate", parseFloat)
+      .option("--e <number>", "East coordinate", parseFloat)
+      .option("--s <number>", "South coordinate", parseFloat)
+      .option("--rows <number>", "Rows in a grid", parseInt)
+      .option("--cols <number>", "Cols in a grid", parseInt)
+      .option("--conn <string>", "Database connection string");
+    return args.parse().opts();
+  }
+}
