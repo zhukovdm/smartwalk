3e568a3b3d59c3579d5db0da9d07008511a7bb72
Dmitry Zhukov <zhukovdm@proton.me>
2023-12-29 16:51:44 +0100

    revert keyword examples to txt

--- data/advice/src/writer.ts
+++ data/advice/src/writer.ts
@@ -3,18 +3,6 @@ import fs from "fs";
 const ASSETS_DIR = "../assets";
 
 export default function writeToFile(keywords: string[]) {
-  const fileName = `${ASSETS_DIR}/advice/keywords.html`;
-  fs.writeFileSync(fileName, `<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>SmartWalk | Examples of keywords</title>
-</head>
-<body>
-<pre>
-${keywords.join("\n")}
-</pre>
-</body>
-</html>
-`);
+  const fileName = `${ASSETS_DIR}/advice/keywords.txt`;
+  fs.writeFileSync(fileName, keywords.join("\n"));
 }
