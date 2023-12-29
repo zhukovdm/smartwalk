cbd09e097c7f756d2c86b137f8f8dfa103d0024b
Dmitry Zhukov <zhukovdm@proton.me>
2023-12-29 13:02:43 +0100

    examples of keywords as html

--- data/advice/src/writer.ts
+++ data/advice/src/writer.ts
@@ -3,6 +3,18 @@ import fs from "fs";
 const ASSETS_DIR = "../assets";
 
 export default function writeToFile(keywords: string[]) {
-  const fileName = `${ASSETS_DIR}/advice/keywords.txt`;
-  fs.writeFileSync(fileName, keywords.join("\n"));
+  const fileName = `${ASSETS_DIR}/advice/keywords.html`;
+  fs.writeFileSync(fileName, `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>SmartWalk | Examples of keywords</title>
+</head>
+<body>
+<pre>
+${keywords.join("\n")}
+</pre>
+</body>
+</html>
+`);
 }
