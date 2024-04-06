f3372bcf5556f6c44f7533d93eafe8e19a7dc8b2
Dmitry Zhukov <zhukovdm@proton.me>
2024-04-06 13:25:12 +0200

    [fix #39] refactored `Program.cs` (follow SoC)

---
+++ app/backend/SmartWalk.Api/Helpers/AppConfigurator.cs
@@ -0,0 +1,121 @@
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using SmartWalk.Application.Entities;
+using SmartWalk.Application.Interfaces;
+using SmartWalk.Infrastructure.Advicer;
+using SmartWalk.Infrastructure.EntityIndex;
+using SmartWalk.Infrastructure.EntityStore;
+using SmartWalk.Infrastructure.RoutingEngine;
+
+namespace SmartWalk.Api;
+
+public static class AppConfigurator
+{
+    private static readonly string _corsPolicy = "SmartWalkCors";
+
+    public static void CreateLogger()
+    {
+        Log.Logger ??= new LoggerConfiguration().WriteTo.Console().CreateLogger();
+    }
+
+    public static WebApplicationBuilder Configure(WebApplicationBuilder builder)
+    {
+        var phase = "Application Builder";
+        Log.Information(phase);
+
+        Log.Information("{Phase}: CORS Policy Definition", phase);
+        builder.Services.AddCors(cors => cors.AddPolicy(_corsPolicy, builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }));
+
+        Log.Information("{Phase}: Authentication Methods", phase);
+        builder.Services
+            .AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+            .AddCertificate();
+
+        Log.Information("{Phase}: Controllers", phase);
+        builder.Services.AddControllers();
+
+        Log.Information("{Phase}: Endpoints API Explorer", phase);
+        builder.Services.AddEndpointsApiExplorer();
+
+        Log.Information("{Phase}: Health Checks", phase);
+        builder.Services.AddHealthChecks();
+
+        Log.Information("{Phase}: Generate Swagger UI", phase);
+        builder.Services.AddSwaggerGen((g) =>
+        {
+            g.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "SmartWalk.Api",
+                Version = "1.0.0",
+                Description = "Web application for keyword-aware walking route search"
+            });
+
+            Directory
+                .GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList()
+                .ForEach(f => g.IncludeXmlComments(f));
+        });
+
+        Log.Information("{Phase}: Advice Context", phase);
+        builder.Services.AddSingleton<IAdviceContext>(new AdviceContext()
+        {
+            KeywordAdvicer = MongoKeywordAdvicer.GetInstance()
+        });
+
+        Log.Information("{Phase}: Entity Context", phase);
+        builder.Services.AddSingleton<IEntityContext>(new EntityContext()
+        {
+            EntityStore = MongoEntityStore.GetInstance()
+        });
+
+        Log.Information("{Phase}: Search Context", phase);
+        builder.Services.AddSingleton<ISearchContext>(new SearchContext()
+        {
+            EntityIndex = MongoEntityIndex.GetInstance(),
+            RoutingEngine = OsrmRoutingEngine.GetInstance()
+        });
+
+        return builder;
+    }
+
+    public static WebApplication Configure(WebApplication app)
+    {
+        var phase = "Application Instance";
+        Log.Information(phase);
+
+        Log.Information("{Phase}: Swagger User Interface", phase);
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger().UseSwaggerUI(u =>
+            {
+                u.SwaggerEndpoint("v1/swagger.yaml", "SmartWalk.Api");
+            });
+        }
+
+        Log.Information("{Phase}: Use CORS Policy", phase);
+        app.UseCors(_corsPolicy);
+
+        Log.Information("{Phase}: Use Authorization", phase);
+        app.UseAuthorization();
+
+        Log.Information("{Phase}: Use Authentization", phase);
+        app.UseAuthentication();
+
+        Log.Information("{Phase}: Map Controllers", phase);
+        app.MapControllers();
+
+        Log.Information("{Phase}: Map Health Checks", phase);
+        app.MapHealthChecks("/healthcheck");
+
+        return app;
+    }
+}
--- app/backend/SmartWalk.Api/Program.cs
+++ app/backend/SmartWalk.Api/Program.cs
@@ -1,86 +1,38 @@
 using System;
-using System.IO;
-using System.Linq;
-using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using SmartWalk.Application.Entities;
-using SmartWalk.Application.Interfaces;
-using SmartWalk.Infrastructure.Advicer;
-using SmartWalk.Infrastructure.EntityIndex;
-using SmartWalk.Infrastructure.EntityStore;
-using SmartWalk.Infrastructure.RoutingEngine;
+using Serilog;
 
 namespace SmartWalk.Api;
 
-public class Program
+public sealed class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        var policy = "SmartWalkCors";
+        var appName = "SmartWalk";
+        AppConfigurator.CreateLogger();
 
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddCors(cors => cors.AddPolicy(policy, builder =>
+        try
         {
-            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-        }));
+            Log.Information("Configuring application ({AppName})", appName);
 
-        builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
+            var builder = AppConfigurator.Configure(WebApplication.CreateBuilder(args));
+            var app = AppConfigurator.Configure(builder.Build());
 
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddHealthChecks();
+            Log.Information("Starting application ({AppName})", appName);
 
-        builder.Services.AddSwaggerGen((g) => {
-            g.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Title = "SmartWalk.Api",
-                Version = "1.0.0",
-                Description = "Web application for keyword-aware walking route search"
-            });
+            app.Run();
 
-            Directory
-                .GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList()
-                .ForEach(f => g.IncludeXmlComments(f));
-        });
-
-        builder.Services.AddSingleton<IAdviceContext>(new AdviceContext()
-        {
-            KeywordAdvicer = MongoKeywordAdvicer.GetInstance()
-        });
-
-        builder.Services.AddSingleton<IEntityContext>(new EntityContext()
-        {
-            EntityStore = MongoEntityStore.GetInstance()
-        });
-
-        builder.Services.AddSingleton<ISearchContext>(new SearchContext()
-        {
-            EntityIndex = MongoEntityIndex.GetInstance(),
-            RoutingEngine = OsrmRoutingEngine.GetInstance()
-        });
-
-        var wapp = builder.Build();
-
-        if (wapp.Environment.IsDevelopment())
-        {
-            wapp.UseSwagger().UseSwaggerUI(u =>
-            {
-                u.SwaggerEndpoint("v1/swagger.yaml", "SmartWalk.Api");
-            });
+            Log.Information("Stopping application ({AppName})", appName);
+            return 0;
         }
-
-        wapp.UseCors(policy)
-//          .UseHttpsRedirection()
-            .UseAuthorization()
-            .UseAuthentication();
-
-        wapp.MapControllers();
-        wapp.MapHealthChecks("/healthcheck");
-
-        wapp.Run();
+        catch (Exception ex)
+        {
+            Log.Fatal("Application crashed with the message: {Message}", ex.Message);
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
