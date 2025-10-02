234ecb40133c6b519d3a1da9141b640b7c7e8365
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2025-10-03 00:06:50 +0200

    temp fix: start without calling mongo service (#73)

--- app/backend/SmartWalk.Infrastructure/Mongo/MongoKeywordAdvicer.cs
+++ app/backend/SmartWalk.Infrastructure/Mongo/MongoKeywordAdvicer.cs
@@ -1,19 +1,20 @@
-using MongoDB.Driver;
+// using System.Linq;
+// using MongoDB.Driver;
 using SmartWalk.Core.Interfaces;
 using SmartWalk.Infrastructure.Mongo.Helpers;
 
 namespace SmartWalk.Infrastructure.Mongo;
 
-using Item = TrieKeywordAdvicer.Item;
+// using Item = TrieKeywordAdvicer.Item;
 
 public sealed class MongoKeywordAdvicer
 {
     public static IKeywordAdvicer GetInstance()
     {
-        var docs = MongoCollectionFactory.GetKeywordCollection()
-            .Find(FilterDefinition<Item>.Empty)
-            .ToEnumerable(); // synchronous!
+        // var docs = MongoCollectionFactory.GetKeywordCollection()
+        //     .Find(FilterDefinition<Item>.Empty)
+        //     .ToEnumerable(); // synchronous!
 
-        return TrieKeywordAdvicer.GetInstance(docs);
+        return TrieKeywordAdvicer.GetInstance([] /* docs */);
     }
 }
